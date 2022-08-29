ed925e7ce2e584fdef1ab42f62e67d1779b71b3b
Fai53 <110114521+Fai53@users.noreply.github.com>
2022-08-29 15:03:18 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './sceens/Home';
+import Register from './sceens/Register';
+import Login from './sceens/Register';
+
+function App() {
+  return (
+    <div>
+    <NavBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/context/AuthContext.js
@@ -0,0 +1,20 @@
+import React,{createContext, useState} from 'react'
+
+
+const AuthContext = createContext();
+AuthProvider = AuthContext.provider;
+
+
+function AuthContext() {
+    const [success, setSuccess]= useState("false");
+    const [error, setError] = useState("null");
+    const [loading, setLoading] = useState("false");
+    const [user, setUser ] = useState("null")
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default AuthContext
