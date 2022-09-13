3450ec1e0e06501cc926f6466e43852285a5b1d9
Fai53 <110114521+Fai53@users.noreply.github.com>
2022-09-13 14:45:26 +0000

    login authorization

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,20 +5,38 @@ const AuthContext = createContext();
 AuthProvider = AuthContext.Provider;
 
 
-function AuthContextProvider() {
+function AuthContextProvider({children}) {
     const [success, setSuccess]= useState("false");
     const [error, setError] = useState("null");
     const [loading, setLoading] = useState("false");
     const [user, setUser ] = useState("null")
     // signup function
     const authRegister = async (username, email, password) =>{
-      await fetch(url, {
-        method:"POST"
+      setLoading(true);
+      await fetch("/api/users/register", {
+        method:"POST",
+        headers: {
+          "content-Type": "application.json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
 
-      }
-
-      )
-    }
+      })
+      .then((res) =>res.json())
+      .then((data) =>{
+        console.log(data);
+        setSuccess(true);
+        setUser(data);
+      })
+      .catch((err) =>{
+        console.log(err);
+        setError(err);
+      });
+      setLoading(false);
+    };
   return (
     <div>
 
